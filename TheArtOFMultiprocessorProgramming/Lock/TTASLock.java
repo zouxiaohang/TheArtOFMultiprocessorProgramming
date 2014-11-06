6c3cb6a29ff9acd7b2f1a51843203c2bdf53278e
邹晓航 <1210603696@qq.com>
2014-11-06 09:40:41 +0800

    添加评注

--- TheArtOFMultiprocessorProgramming/Lock/TTASLock.java
+++ TheArtOFMultiprocessorProgramming/Lock/TTASLock.java
@@ -5,12 +5,19 @@ class TTASLock{
 	
 	void lock(){
 		while(true){
+			/*
+			*使用local spin技术，减少了无谓的cache coherence和bus占用
+			*/
 			while(flag.get()){}//local spin
 			if(!flag.getAndSet(true))
 				return;
 		}
 	}
 	void unlock(){
+		/*
+		*此时会使所有local spin的线程cache 失效，大量线程改写flag，
+		*造成high contention
+		*/
 		flag.set(false);
 	}
 }
