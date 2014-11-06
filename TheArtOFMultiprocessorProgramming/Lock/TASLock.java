466cc03c54c826cc8ee5267b651ddff8f48609b3
邹晓航 <1210603696@qq.com>
2014-11-06 09:37:57 +0800

    添加评注

--- TheArtOFMultiprocessorProgramming/Lock/TASLock.java
+++ TheArtOFMultiprocessorProgramming/Lock/TASLock.java
@@ -3,6 +3,12 @@ import java.util.concurrent.atomic.*;
 class TASLock{
 	private AtomicBoolean flag = new AtomicBoolean(false);
 	void lock(){
+		/*（1）flag的每一次getAndSet都会重写其cache中的值，
+		*由于flag是其他线程共享的因此会invalidate其他线程的cache，
+		*造成无谓的cache coherence，降低性能。
+		*（2）又由于每次都重写flag使得写的过程中占用了bus，造成bus traffic jam，
+		*delay了其他线程，甚至是delay了需要release lock的线程。
+		*/
 		while(flag.getAndSet(true)){}
 	}
 	void unlock(){
