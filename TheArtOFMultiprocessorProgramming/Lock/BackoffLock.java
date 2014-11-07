307d934a99fed0e622a7bcd7aa69d9ecb8aaae95
邹晓航 <1210603696@qq.com>
2014-11-07 15:19:09 +0800

    添加扩展性

--- TheArtOFMultiprocessorProgramming/Lock/BackoffLock.java
+++ TheArtOFMultiprocessorProgramming/Lock/BackoffLock.java
@@ -2,12 +2,16 @@ import java.util.concurrent.atomic.*;
 
 public class BackoffLock{
 	private AtomicBoolean state = new AtomicBoolean(false);
-	private static final int MIN_DELAY = 10;//you choose a appropriate value
-	private static final int MAX_DELAY = 100;//you choose a appropriate value
+	private final int MIN_DELAY;//you choose a appropriate value
+	private final int MAX_DELAY;//you choose a appropriate value
 	/*
 	*这个锁的性能与MIN_DELAY、MAX_DELAY的值的选取有直接的关系，
 	*且所有的线程都在同一个location上spin，使得当成功lock时造成cache-coherence traffic
 	*/
+	public BackoffLock(int min, int max){
+		MIN_DELAY = min;
+		MAX_DELAY = max;
+	}
 	public void lock(){
 		Backoff backoff = new Backoff(MIN_DELAY, MAX_DELAY);
 		while(true){
