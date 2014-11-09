2d1a10bf9a8bbd4e453db26d9e76d83f369f30a2
邹晓航 <1210603696@qq.com>
2014-11-09 16:01:52 +0800

    添加评注

--- TheArtOFMultiprocessorProgramming/Lock/MCSLock.java
+++ TheArtOFMultiprocessorProgramming/Lock/MCSLock.java
@@ -20,20 +20,25 @@ public class MCSLock{
 	public void lock(){
 		QNode node = myNode.get();
 		QNode pred = tail.getAndSet(node);
-		if(pred != null){
+		if(pred != null){//此时queue中有node不能直接拿锁
 			node.locked = true;
-			pred.next = node;
-			while(node.locked){}
+			pred.next = node;//将自己加入到queue中
+			/*
+			*缺点是需要前继来改变自己的locked域，可能会造成unbounded wait
+			*/
+			while(node.locked){}//在自己的locked域上spin
 		}
 	}
 	public void unlock(){
 		QNode node = myNode.get();
-		if(node.next == null){
-			if(tail.compareAndSet(node, null))
+		if(node.next == null){//此时我的node没有后继
+			if(tail.compareAndSet(node, null))//判断此时queue中是否只有我一个node
 				return;
+			//此时tail不指向我的node说明有新的node正在加入queue中
+			//此时等待我的后继node完成add操作，将我的next域设为后继node
 			while(node.next == null){}
 		}
 		node.next.locked = false;
-		node.next = null;
+		node.next = null;//将我的node从queue中dequeue
 	}
 }
