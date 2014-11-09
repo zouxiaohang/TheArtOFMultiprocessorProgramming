a52bf686c69a42b629d85039cb46aafb201ada2a
邹晓航 <1210603696@qq.com>
2014-11-09 15:35:04 +0800

    添加评注

--- TheArtOFMultiprocessorProgramming/Lock/CLHLock.java
+++ TheArtOFMultiprocessorProgramming/Lock/CLHLock.java
@@ -10,6 +10,7 @@ public class CLHLock{
 	private ThreadLocal<QNode> myNode;
 	
 	public CLHLock(){
+		//原书有误，tail初始化的时候应该指向一个QNode否则第一个加入的thread就不能重用先前的QNode了
 		//tail = new AtomicReference<QNode>(null);
 		tail = new AtomicReference<QNode>(new QNode());
 		myNode = new ThreadLocal<QNode>(){
@@ -29,11 +30,16 @@ public class CLHLock{
 		node.locked = true;
 		QNode pred = tail.getAndSet(node);
 		myPred.set(pred);
+		/*
+		*无限制的wait存在缺陷，且实在pred node的locked域上spin，
+		*在cache-less NUMA架构的环境上性能会变得poor
+		*/
 		while(pred.locked){}
 	}
 	public void unlock(){
 		QNode node = myNode.get();
 		node.locked = false;
+		//重用pred node
 		myNode.set(myPred.get());
 	}
 }
