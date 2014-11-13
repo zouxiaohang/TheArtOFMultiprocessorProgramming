3775ca840c1f7bd347e186e3de1bea03abf665e1
邹晓航 <1210603696@qq.com>
2014-11-13 09:35:59 +0800

    添加评注

--- TheArtOFMultiprocessorProgramming/Lock/CompositeLock.java
+++ TheArtOFMultiprocessorProgramming/Lock/CompositeLock.java
@@ -17,6 +17,7 @@ public class CompositeLock{
 	private final int SIZE;
 	private final int MIN_BACKOFF;
 	private final int MAX_BACKOOF;
+	//避免ABA问题
 	AtomicStampedReference<QNode> tail;
 	QNode[] waiting;
 	Random random;
@@ -46,14 +47,17 @@ public class CompositeLock{
 		QNode currTail;
 		int []currStamp = {0};
 		while(true){
+			//如果此时node状态为free(没线程占用且不在queue中)
 			if(node.state.compareAndSet(State.FREE, State.WAITING)){
 				return node;
 			}
 			currTail = tail.get(currStamp);
 			State state = node.state.get();
+			//如果node没线程占用但在queue中
 			if(state == State.ABORTED || state == State.RELEASE){
-				if(node == currTail){
+				if(node == currTail){//此时node为queue tail
 					QNode myPred = null;
+					//如果tail为aborted则redirect至pred，否则tail为release，设为null
 					if(state == State.ABORTED){
 						myPred = node.pred;
 					}
@@ -63,6 +67,7 @@ public class CompositeLock{
 					}
 				}
 			}
+			//如果node为waiting状态则wait
 			backoff.backoff();
 			if(timeout(patience, startTime)){
 				throw new TimeoutException();
@@ -78,28 +83,29 @@ public class CompositeLock{
 				node.state.set(State.FREE);
 				throw new TimeoutException();
 			}
+		//在此间隙，tail没被其他线程更改，则将node设为tail并返回tail
 		}while(!tail.compareAndSet(currTail, node, currStamp[0], currStamp[0] + 1));
 		return currTail;
 	}
 	private void waitForPredecessor(QNode pred, QNode node, long startTime, long patience) throws TimeoutException{
 		int []stamp = {0};
-		if(pred == null){
+		if(pred == null){//表明node是queue head
 			myNode.set(node);
 			return ;
 		}
 		State predState = pred.state.get();
-		while(predState != State.RELEASE){
-			if(predState == State.ABORTED){
+		while(predState != State.RELEASE){//等待pred被线程设为release
+			if(predState == State.ABORTED){//pred等待超时
 				QNode temp = pred;
-				pred = pred.pred;
+				pred = pred.pred;//将超时的node从queue中去除
 				temp.state.set(State.FREE);
 			}
-			if(timeout(patience, startTime)){
-				node.pred = pred;
+			if(timeout(patience, startTime)){//node超时
+				node.pred = pred;//为了让后面的node跳过自己
 				node.state.set(State.ABORTED);
 				throw new TimeoutException();
 			}
-			predState = pred.state.get();
+			predState = pred.state.get();//再次读取state
 		}
 		pred.state.set(State.FREE);
 		myNode.set(node);
@@ -110,8 +116,11 @@ public class CompositeLock{
 		long startTime = System.currentTimeMillis();
 		Backoff backoff = new Backoff(MIN_BACKOFF, MAX_BACKOOF);
 		try{
+			//从waiting中拿到一个node
 			QNode node = acquireQNode(backoff, startTime, patience);
+			//将这个node加入queue中
 			QNode pred = spliceQNode(node, startTime, patience);
+			//等待node成为queue head
 			waitForPredecessor(pred, node, startTime, patience);
 			return true;
 		}catch (TimeoutException e) {
