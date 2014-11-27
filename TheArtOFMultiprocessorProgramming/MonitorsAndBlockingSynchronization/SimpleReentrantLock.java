b5626ef35e59d459d272b4dbe17802eb09d56acd
邹晓航 <1210603696@qq.com>
2014-11-27 19:01:12 +0800

    add SimpleReentrantLock

---
+++ TheArtOFMultiprocessorProgramming/MonitorsAndBlockingSynchronization/SimpleReentrantLock.java
@@ -0,0 +1,48 @@
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class SimpleReentrantLock{
+	Lock lock;
+	Condition condition;
+	long owner;
+	int holdCount;
+	
+	public SimpleReentrantLock(){
+		lock = new ReentrantLock();
+		condition = lock.newCondition();
+		owner = 0;
+		holdCount = 0;
+	}
+	public void lock() throws InterruptedException{
+		long me = Thread.currentThread().getId();
+		lock.lock();
+		try{
+			if(owner == me){
+				++holdCount;
+				return;
+			}
+			while(holdCount != 0){
+				condition.await();
+			}
+			owner = me;
+			holdCount = 1;
+		}finally{
+			lock.unlock();
+		}
+	}
+	public void unlock(){
+		lock.lock();
+		try{
+			if(holdCount == 0 || owner != Thread.currentThread().getId()){
+				throw new IllegalMonitorStateException();
+			}
+			--holdCount;
+			if(holdCount == 0){
+				condition.signal();
+			}
+		}finally{
+			lock.unlock();
+		}
+	}
+}
